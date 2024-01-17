24cf0eca41b00d1203cee03671b81c68fbd525f1
Elshaday_Asfaw <71713154+ElshadayD@users.noreply.github.com>
2024-01-17 11:04:59 +0300

    Update Two_sums.py

--- Two_sums.py
+++ Two_sums.py
@@ -1,3 +1,10 @@
+# question
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
+
+# Answer
+
 class Solution(object):
     def twoSum(self, nums, target):
         temp= [0 for x in range(2)]

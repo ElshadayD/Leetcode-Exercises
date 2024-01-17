4dcab471418b411836fa083393e80843c91cdd78
Elshaday_Asfaw <71713154+ElshadayD@users.noreply.github.com>
2024-01-17 11:13:31 +0300

    Update Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -1,3 +1,8 @@
+# Question
+#Write a function to find the longest common prefix string amongst an array of strings.
+#If there is no common prefix, return an empty string "".
+
+#Answer
 class Solution(object):
     def longestCommonPrefix(self, strs):
         if len(strs)==0:
